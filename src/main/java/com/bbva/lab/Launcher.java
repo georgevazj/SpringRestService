f2c3c5e5120ed9312eb28de3ddf3f040817fe19b
Jorge <georgevazj@gmail.com>
2016-12-21 07:52:34 +0100

    Actualizada la configuracion CORS

--- src/main/java/com/bbva/lab/Launcher.java
+++ src/main/java/com/bbva/lab/Launcher.java
@@ -3,16 +3,7 @@ package com.bbva.lab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.Validator;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.*;
-
-import java.util.List;
 
 /**
  * Created by jorge on 01/12/2016.
